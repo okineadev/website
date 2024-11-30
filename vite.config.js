8ce636eca623f3447ab96e173e9c7f3c1d1336d7
Okinea Dev <okineadev@users.noreply.github.com>
2024-11-30 09:53:42 +0200

    feat: use autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import liveReload from "vite-plugin-live-reload";
+import { defineConfig } from "vite";
+// import liveReload from "vite-plugin-live-reload";
 import { createHtmlPlugin } from "vite-plugin-html";
 import { compression as viteCompression } from "vite-plugin-compression2";
 import Sitemap from "vite-plugin-sitemap";
-import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
 
 /**
  * Google Tag Manager script to be included in the HTML.
  *
  * This script asynchronously loads the Google Tag Manager library and initializes it with the provided tracking ID.
  * It also sets up the `gtag` function to push events to the `dataLayer`.
- *
- * @constant {string} googleTagScript
  */
 const googleTagScript = `
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-0TM7HRVX06"></script>
@@ -30,13 +29,10 @@ const googleTagScript = `
  *
  * @file vite.config.js
  * @description This configuration file sets up Vite with various plugins and settings for development and production environments.
- *
- * @plugin liveReload - Enables live reloading during development.
- * @plugin createHtmlPlugin - Generates HTML files with optional minification and script injection.
  */
 export default defineConfig({
 	plugins: [
-		liveReload(),
+		// liveReload(),
 		createHtmlPlugin({
 			minify: true,
 			inject: {
@@ -51,6 +47,11 @@ export default defineConfig({
 		}),
 		Sitemap({ hostname: "https://okinea.dev" }),
 	],
+	css: {
+		postcss: {
+			plugins: [autoprefixer()],
+		},
+	},
 	root: "src",
 	server: {
 		open: true,
