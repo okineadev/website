ff25adf083ba202a892551395459d08b8e3fa3ef
Okinea Dev <hi@okinea.dev>
2024-12-24 18:05:42 +0200

    chore: use redirect for my telegram profile

--- src/socials.ts
+++ src/socials.ts
@@ -3,7 +3,7 @@ import type { Socials } from './types'
 const socials: Socials = {
 	GitHub: 'https://github.com/okineadev',
 	Unsplash: 'https://unsplash.com/@okineadev',
-	Telegram: 'https://t.me/okineadev',
+	Telegram: 'https://telegram.okinea.dev',
 	Medium: 'https://okineadev.medium.com',
 	Discord: 'https://discord.com/users/okineadev',
 	'Stack Overflow': 'https://stackoverflow.com/users/21165921/okinea-dev',
