ec6ade9b604aa8bb59bfbd7e4df4d8b5b8807bc5
Okinea Dev <hi@okinea.dev>
2025-02-06 21:02:14 +0200

    feat: ✨ add new projects to projects sections

--- src/projects.ts
+++ src/projects.ts
@@ -2,21 +2,33 @@ import type { Project } from './types'
 
 const myProjects: Project[] = [
 	{
+		repo: 'okineadev/material-icons-figma-plugin',
+		description: '🍱 Material Icons for Figma',
+	},
+	{
+		repo: 'okineadev/cli-tips',
+		description: '💡 CLI tool that provides useful tips and commands for Linux users',
+	},
+	{
+		repo: 'just-serverless/contributors-table',
+		description: '😎 Service for embedding the table of contributors in the readme of the project'
+	},
+	{
+		repo: 'okineadev/okineadev-website',
+		description: '🌐 My personal portfolio website showcasing my projects, blog posts and contacts',
+	},
+	{
 		repo: 'okineadev/dotload',
 		description: '🌠 Simple tool for installing dotfiles on any machine!',
 	},
 	{
-		repo: 'okineadev/cli-tips',
-		description: '💡 CLI tool that provides useful tips\nand commands for Linux users',
-	},
-	{
-		repo: 'okineadev/okineadev-website',
-		description: 'This cool website',
-	},
-	{
 		repo: 'okineadev/platform-tools-installer-gui',
-		description: '📥 Unofficial platform-tools GUI installer\n(adb, fastboot) for Windows™',
+		description: '📥 Unofficial platform-tools GUI installer (adb, fastboot) for Windows™',
 	},
+	{
+		repo: 'cli-stuff/platform-tools-installer-windows',
+		description: '🏄‍♂️ Command line script to easily install platform-tools (adb, fastboot) on Windows™',
+	}
 ]
 
 export default myProjects
