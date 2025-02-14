8254d4e366d529701d2ccc827dc0b52b20a5a31e
Okinea Dev <hi@okinea.dev>
2025-02-14 20:02:46 +0100

    fix: 🐛 correct `Sitemap` hostname format
    * Updated the `Sitemap` configuration to include the protocol in the hostname.

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
 		}),
 		// @ts-expect-error
 		viteCompression({ filter: /\.(js|css|html)$/i }),
-		Sitemap({ hostname: DOMAIN }),
+		Sitemap({ hostname: `https://${DOMAIN}` }),
 	],
 	css: {
 		postcss: {
