365ab52e6dd9b6a87a3a1c93fa67161f154272b4
Okinea Dev <hi@okinea.dev>
2025-08-17 12:24:00 +0200

    feat: add Catppuccin cat to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,15 @@ import Cursorphobic from './Other/Cursorphobic.vue'
 		<Cursorphobic multiplier="0.3"
 			><a href="https://github.com/okineadev/okineadev-website">Source Code</a></Cursorphobic
 		>
+
+		<img src="../assets/cat.svg" style="margin-top: 40px" width="600" height="75" />
 	</footer>
 </template>
 
 <style lang="scss" scoped>
 footer {
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	margin-top: 25px;
 	margin-bottom: 70px;
